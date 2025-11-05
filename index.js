3bb2c9bb8a294c48067868374e7c374b68946e44
mikkelbnoergaard <s224562@dtu.dk>
2025-11-05 20:55:22 +0100

    wow added randomness

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("This is funny!"));
+app.get("/", (req, res) => {
+    const messages = [
+        "<!DOCTYPE html><html><head><meta charset='UTF-8'><title>Random</title></head><body><h1>Tobi lugter af nums</h1></body></html>",
+        "<!DOCTYPE html><html><head><meta charset='UTF-8'><title>Random</title></head><body><h1>Welcome to Ponger</h1></body></html>"
+    ];
+
+    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+    res.send(randomMessage);
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
